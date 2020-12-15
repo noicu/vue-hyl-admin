651239bcdf634b6c6b080aa828a6a2ce8bde53b3
Tovi <1018496570@qq.com>
2020-12-15 16:58:25 +0800

    perf(tabs): perf multiple-tabs

--- src/components/Menu/src/BasicMenu.tsx
+++ src/components/Menu/src/BasicMenu.tsx
@@ -241,6 +241,7 @@ export default defineComponent({
           onOpenChange={handleOpenChange}
           class={unref(getMenuClass)}
           onClick={handleMenuClick}
+          subMenuOpenDelay={0.2}
           {...unref(getInlineCollapseOptions)}
         >
           {{
